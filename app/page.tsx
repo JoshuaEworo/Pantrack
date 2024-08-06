544d177c6a1bb9fb8eb62c74f0de230bb6fdab85
Joshua Eworo <eworojoshua@gmail.com>
2024-08-06 15:40:31 -0500

    Fixed Mobile Chrome text-colors

--- app/page.tsx
+++ app/page.tsx
@@ -60,20 +60,20 @@ export default function Login({
 
       <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2 mx-auto">
         <form className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground">
-          <label className="text-md" htmlFor="email">
+          <label className="text-md text-white" htmlFor="email">
             Email
           </label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="rounded-md px-4 py-2 bg-inherit border mb-6 text-neutral-200"
             name="email"
             placeholder="you@example.com"
             required
           />
-          <label className="text-md" htmlFor="password">
+          <label className="text-md text-white" htmlFor="password">
             Password
           </label>
           <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
+            className="rounded-md px-4 py-2 bg-inherit border mb-6 text-neutral-200"
             type="password"
             name="password"
             placeholder="••••••••"
@@ -81,14 +81,14 @@ export default function Login({
           />
           <SubmitButton
             formAction={signIn}
-            className="bg-green-700 rounded-md px-4 py-2 text-foreground mb-2"
+            className="bg-green-700 rounded-md px-4 py-2 text-white mb-2"
             pendingText="Signing In..."
           >
             Sign In
           </SubmitButton>
           <SubmitButton
             formAction={signUp}
-            className="border border-foreground/20 rounded-md px-4 py-2 text-foreground mb-2"
+            className="border border-foreground/20 rounded-md px-4 py-2 text-white mb-2"
             pendingText="Signing Up..."
           >
             Sign Up
